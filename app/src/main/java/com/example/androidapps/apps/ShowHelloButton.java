cbabc0991783c349c649aadd2b611a928d50ad42
Jorim <jorimpablico2004@gmail.com>
2024-10-01 14:58:44 +0800

    feat: add ShowHelloButton

--- app/src/main/java/com/example/androidapps/apps/ShowHelloButton.java
+++ app/src/main/java/com/example/androidapps/apps/ShowHelloButton.java
@@ -1,6 +1,8 @@
 package com.example.androidapps.apps;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,21 @@ import com.example.androidapps.R;
 
 public class ShowHelloButton extends AppCompatActivity {
 
+    Button btn_showHello;
+    TextView showHello;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_show_hello_button);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn_showHello = findViewById(R.id.btn_show_hello);
+        showHello = findViewById(R.id.show_hello);
+
+        btn_showHello.setOnClickListener(v -> {
+            showHello.setText("Hello World");
         });
+
+
     }
 }
