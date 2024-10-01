3b4769208309f09c98ef45f291ac6fad2fb9c7e1
Jorim <jorimpablico2004@gmail.com>
2024-10-01 15:00:38 +0800

    feat: add counter

--- app/src/main/java/com/example/androidapps/apps/Counter.java
+++ app/src/main/java/com/example/androidapps/apps/Counter.java
@@ -1,6 +1,8 @@
 package com.example.androidapps.apps;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,23 @@ import com.example.androidapps.R;
 
 public class Counter extends AppCompatActivity {
 
+    Button btnAddCount;
+    TextView showCounter;
+    int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_counter);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnAddCount = findViewById(R.id.btn_add_count);
+        showCounter = findViewById(R.id.show_counter);
+
+        btnAddCount.setOnClickListener(v -> {
+            counter++;
+
+            showCounter.setText("Counter: " + counter);
         });
+
     }
 }
