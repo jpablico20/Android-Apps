324fabcea628b50dcf6e206e014b8b00cbffd77a
Jorim <jorimpablico2004@gmail.com>
2024-10-01 15:03:22 +0800

    feat: add USD to PHP converter

--- app/src/main/java/com/example/androidapps/apps/CurrencyConverter.java
+++ app/src/main/java/com/example/androidapps/apps/CurrencyConverter.java
@@ -1,6 +1,9 @@
 package com.example.androidapps.apps;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +15,29 @@ import com.example.androidapps.R;
 
 public class CurrencyConverter extends AppCompatActivity {
 
+    EditText inputAmount;
+    Button btnConvert;
+    TextView showAmount;
+    double pesoRate = 56.21, getAmount, phpAmount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_currency_converter);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        inputAmount =findViewById(R.id.input_amount);
+        btnConvert =findViewById(R.id.btn_convert);
+        showAmount =findViewById(R.id.show_amount);
+
+        btnConvert.setOnClickListener(v -> {
+
+        getAmount = Double.parseDouble(inputAmount.getText().toString());
+        phpAmount = getAmount * pesoRate;
+
+        showAmount.setText("Amount in PHP: " + phpAmount);
+
         });
+
+
     }
 }
