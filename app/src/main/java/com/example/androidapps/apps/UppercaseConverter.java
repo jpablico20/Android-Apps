e4e44c019322dc1b4edc34da7e2e3c7a5e5c76bc
Jorim <jorimpablico2004@gmail.com>
2024-10-04 15:44:10 +0800

    feat: add Uppercase converter

---
+++ app/src/main/java/com/example/androidapps/apps/UppercaseConverter.java
@@ -0,0 +1,39 @@
+package com.example.androidapps.apps;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.androidapps.R;
+
+public class UppercaseConverter extends AppCompatActivity {
+
+    EditText inputText;
+    Button btnUppercase;
+    TextView convertUppercase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uppercase_converter);
+
+        inputText = findViewById(R.id.input_text);
+        btnUppercase = findViewById(R.id.btn_uppercase);
+        convertUppercase = findViewById(R.id.show_uppercase);
+
+        btnUppercase.setOnClickListener(v -> {
+            String getText = inputText.getText().toString();
+
+            convertUppercase.setText(getText.toUpperCase());
+        });
+
+
+    }
+}
