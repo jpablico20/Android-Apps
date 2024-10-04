84ed2b5cc808ad9da8f74526a65da57e2d6450ce
Jorim <jorimpablico2004@gmail.com>
2024-10-04 15:35:11 +0800

    feat: add scroll view and new button

--- app/src/main/java/com/example/androidapps/MainActivity.java
+++ app/src/main/java/com/example/androidapps/MainActivity.java
@@ -18,25 +18,37 @@ import com.example.androidapps.apps.CurrencyConverter;
 import com.example.androidapps.apps.DisplayHelloWorld;
 import com.example.androidapps.apps.DisplayName;
 import com.example.androidapps.apps.EvenOddChecker;
+import com.example.androidapps.apps.FactorialCalculator;
 import com.example.androidapps.apps.FahrenheitToCelsius;
+import com.example.androidapps.apps.GuessNumber;
+import com.example.androidapps.apps.MultipleChecker;
 import com.example.androidapps.apps.MultiplicationTable;
 import com.example.androidapps.apps.PalindromeChecker;
+import com.example.androidapps.apps.RandomNumberGenerator;
+import com.example.androidapps.apps.ShoppingList;
 import com.example.androidapps.apps.ShowDate;
 import com.example.androidapps.apps.ShowHelloButton;
 import com.example.androidapps.apps.ShowUserName;
 import com.example.androidapps.apps.SimpleForm;
 import com.example.androidapps.apps.SimpleLogin;
+import com.example.androidapps.apps.TemperatureConverter;
 import com.example.androidapps.apps.TextLength;
+import com.example.androidapps.apps.TodoManager;
+import com.example.androidapps.apps.UppercaseConverter;
 import com.example.androidapps.apps.UserAge;
 import com.example.androidapps.apps.UserGreeting;
+import com.example.androidapps.apps.WordCounter;
 import com.example.androidapps.apps.WordReverser;
+import com.example.androidapps.apps.WordShuffler;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnDisplayHelloWorld, btnshowHelloButton, btnDisplayname, btnCounter,
     btnSimpleForm, btnUserAge, btnUserGreeting, btnCalculator, btnTextLength, btnCurrencyConverter,
     btnEvenOddChecker, btnWordReverser, btnShowUsername, btnShowDate, btnMultiplicationTable,
-    btnSimpleLogin, btnFahrenheitToCelsius, btnBookmark, btnCharacterCounter, btnPalindrome;
+    btnSimpleLogin, btnFahrenheitToCelsius, btnBookmark, btnCharacterCounter, btnPalindrome,
+    btnTemperature, btnShoppingList, btnFactorial, btnTodoManager, btnGuessNumber, btnWordCounter,
+    btnRandomNum, btnMultipleChecker, btnUppercase, btnWordShuffler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +75,17 @@ public class MainActivity extends AppCompatActivity {
         btnBookmark = findViewById(R.id.btn_bookmark);
         btnCharacterCounter = findViewById(R.id.btn_character_count);
         btnPalindrome = findViewById(R.id.btn_palindrome);
+        btnTemperature = findViewById(R.id.btn_temperature);
+        btnShoppingList = findViewById(R.id.btn_shopping_list);
+        btnFactorial = findViewById(R.id.btn_factorial);
+        btnTodoManager = findViewById(R.id.btn_todo_manager);
+        btnGuessNumber = findViewById(R.id.btn_guess_number);
+        btnWordCounter = findViewById(R.id.btn_word_counter);
+        btnRandomNum = findViewById(R.id.btn_random_number);
+        btnMultipleChecker = findViewById(R.id.btn_multiple_checker);
+        btnUppercase = findViewById(R.id.btn_uppercase_converter);
+        btnWordShuffler = findViewById(R.id.btn_shuffle_word);
+
 
         btnDisplayHelloWorld.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, DisplayHelloWorld.class);
@@ -163,5 +186,57 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, PalindromeChecker.class);
             startActivity(intent);
         });
+
+        btnTemperature.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TemperatureConverter.class);
+            startActivity(intent);
+        });
+
+        btnShoppingList.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ShoppingList.class);
+            startActivity(intent);
+        });
+
+        btnFactorial.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, FactorialCalculator.class);
+            startActivity(intent);
+        });
+
+        btnTodoManager.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TodoManager.class);
+            startActivity(intent);
+        });
+
+        btnGuessNumber.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GuessNumber.class);
+            startActivity(intent);
+        });
+
+        btnWordCounter.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, WordCounter.class);
+            startActivity(intent);
+        });
+
+        btnRandomNum.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RandomNumberGenerator.class);
+            startActivity(intent);
+        });
+
+        btnMultipleChecker.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MultipleChecker.class);
+            startActivity(intent);
+        });
+
+        btnUppercase.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, UppercaseConverter.class);
+            startActivity(intent);
+        });
+
+        btnWordShuffler.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, WordShuffler.class);
+            startActivity(intent);
+        });
+
+
     }
 }
